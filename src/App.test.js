32617d1e1196f0cbd000760b94a6bf4873700597
Moreen <moreenkim36@gmail.com>
2020-09-15 09:30:54 +0300

    set up counter clock tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,14 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
+test('renders without error', () => {
   const wrapper = shallow(<App />);
 });
+
+test('renders increment button', () => {});
+
+test('renders counter display', () => {});
+
+test('counter starts at 0', () => {});
+
+test('clicking button increments counter display', () => {});
